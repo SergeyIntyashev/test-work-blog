c4cc1f8abb7cbef003f6a890bf6d83300d411bc6
Sergey Intyashev <seintyashev@mail.ru>
2022-08-26 20:35:19 +0400

    Implemented views for create common user, login and logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from config.views import BaseView
+from users.serializers import LoginSerializer, LogoutSerializer, \
+    CreateCommonUserSerializer
+
+
+class CreateCommonUser(BaseView):
+    """
+    Создания простого пользователя
+    """
+    permission_classes = [AllowAny]
+    serializer_class = CreateCommonUserSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(BaseView):
+    """
+    Авторизация пользователя и получение JWT токена
+    """
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class LogoutAPIView(BaseView):
+    """
+    Logout пользователя и занесение токена в black list
+    """
+    serializer_class = LogoutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
