89eb2a7a54f79a9dcc1f108604c4abae0239f04a
Sergey Intyashev <seintyashev@mail.ru>
2022-08-30 11:30:44 +0400

    Fix path for sign-in

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from users.views import CreateCommonUser, LoginAPIView, LogoutAPIView
 
 urlpatterns = [
-    path('users/', CreateCommonUser.as_view(), name='account-create'),
+    path('sing-in/', CreateCommonUser.as_view(), name='account-create'),
     path('login/', LoginAPIView.as_view(), name='token-login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
