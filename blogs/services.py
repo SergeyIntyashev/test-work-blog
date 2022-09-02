18eac99cedc8209075b61474965fba9fafb36c78
Sergey Intyashev <seintyashev@mail.ru>
2022-09-03 01:49:40 +0400

    Remove unused methods

--- blogs/services.py
+++ blogs/services.py
@@ -1,8 +1,7 @@
 from rest_framework import status, exceptions
 from rest_framework.generics import get_object_or_404
 
-from blogs.models import Posts, Blogs
-from users.models import CustomUser
+from blogs.models import Posts
 
 
 def check_and_get_post(post_id: int, blog_id: int) -> Posts:
@@ -27,26 +26,3 @@ def add_like_to_post(post: Posts) -> None:
     """
     post.likes += 1
     post.save()
-
-
-def get_added_authors_to_blog(authors: list[CustomUser],
-                              current_user: CustomUser,
-                              blog: Blogs) -> list[CustomUser]:
-    blog_authors = blog.authors
-
-    for author in authors:
-        if (author != current_user) or (author not in blog_authors):
-            blog_authors.add(author)
-
-    return blog_authors
-
-
-def get_added_subscriptions_to_blog(current_user: CustomUser,
-                                    blog: Blogs) -> list[CustomUser]:
-
-    blog_subscriptions = blog.subscriptions
-
-    if current_user not in blog_subscriptions:
-        blog_subscriptions.add(current_user)
-
-    return blog_subscriptions
