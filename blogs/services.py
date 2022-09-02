62f9a91d535f402c0427e473631c4e80f4a787e6
Sergey Intyashev <seintyashev@mail.ru>
2022-09-02 18:35:17 +0400

    Remove unused method. Add select related

--- blogs/services.py
+++ blogs/services.py
@@ -1,26 +1,15 @@
 from rest_framework import status, exceptions
 from rest_framework.generics import get_object_or_404
-from rest_framework.request import Request
 
-from blogs.models import Blogs, Posts
-from users.models import CustomUser
+from blogs.models import Posts
 
 
-def user_can_publish_post(blog_id: int, user: CustomUser) -> bool:
-    """
-    Проверяет является ли пользователь владельцем или автором блога
-    """
-    blog = get_object_or_404(Blogs, id=blog_id)
-
-    return bool((blog.owner == user) or (user in blog.authors.all()))
-
-
-def check_and_get_post(post_id: int, blog_id: int, request: Request) -> Posts:
+def check_and_get_post(post_id: int, blog_id: int) -> Posts:
     """
     Проверяет пост на наличие и соответствие его блогу,
     если пост найден и принадлежит блогу возвращает его
     """
-    post = get_object_or_404(Posts, id=post_id)
+    post = get_object_or_404(Posts.objects.select_related('blog'), id=post_id)
 
     if not post.blog.id == blog_id:
         exceptions.PermissionDenied(
