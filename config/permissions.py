526628d09f78137bc0382cc3f2d3c35b7f51ef26
Sergey Intyashev <seintyashev@mail.ru>
2022-09-02 16:08:16 +0400

    Add is authors or blog owner permission

--- config/permissions.py
+++ config/permissions.py
@@ -18,7 +18,7 @@ class IsAuthenticatedAndAuthor(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return bool(request.user and request.user.is_authenticated
-                    and (request.user in obj.authors))
+                    and (request.user in obj.authors.all()))
 
 
 class IsAdminUser(BasePermission):
@@ -28,3 +28,13 @@ class IsAdminUser(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_admin)
+
+
+class IsAuthorOrBlogOwner(BasePermission):
+    """
+    Разрешает доступ автору или владельцу блога, которому принадлежит пост
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return bool((request.user in obj.blog.authors.all())
+                    or (obj.blog.owner == request.user))
