ac99fddae8f7706f3fdc3048e4d097c0ec97cf35
Sergey Intyashev <seintyashev@mail.ru>
2022-09-05 20:13:11 +0400

    Use views from lib

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import token_refresh, \
+    token_obtain_pair, token_blacklist
 
-from users.views import CreateCommonUser, LoginAPIView, LogoutAPIView
+from users.views import CreateCommonUser
 
 urlpatterns = [
     path('sign-in', CreateCommonUser.as_view(), name='account-create'),
-    path('login', LoginAPIView.as_view(), name='token-login'),
-    path('logout', LogoutAPIView.as_view(), name='logout'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+    path('login', token_obtain_pair, name='login'),
+    path('logout', token_blacklist, name='logout'),
+    path('token/refresh', token_refresh, name='token-refresh'),
 ]
