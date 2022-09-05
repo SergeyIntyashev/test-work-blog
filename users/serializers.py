5c7aabca9396150cad4d67db16d67504dc657b39
Sergey Intyashev <seintyashev@mail.ru>
2022-09-05 20:12:53 +0400

    Remove unused views and serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,78 +1,10 @@
-from django.contrib.auth.models import update_last_login
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from django.db import IntegrityError, transaction
 from rest_framework import serializers
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.settings import api_settings as settings
-from rest_framework_simplejwt.exceptions import TokenError
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import CustomUser
-
-
-class LoginSerializer(serializers.Serializer):
-    """
-    Сериализатор для аутентификации пользователя
-    """
-    username = serializers.CharField(write_only=True)
-    password = serializers.CharField(write_only=True)
-    refresh = serializers.CharField(read_only=True)
-    access = serializers.CharField(read_only=True)
-    id = serializers.IntegerField(read_only=True)
-    is_admin = serializers.BooleanField(read_only=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'password')
-
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-
-        try:
-            user = CustomUser.objects.get(username=username)
-        except CustomUser.DoesNotExist:
-            raise AuthenticationFailed('Invalid credentials, try again')
-
-        if not user.check_password(password):
-            raise AuthenticationFailed('Invalid credentials, try again')
-
-        if not user.is_active:
-            raise AuthenticationFailed('User is blocked, contact Admin')
-
-        update_last_login(None, user)
-
-        tokens = RefreshToken.for_user(user)
-
-        return {
-            'username': username,
-            'is_admin': user.is_admin,
-            'refresh': str(tokens),
-            'access': str(tokens.access_token)
-        }
-
-
-class LogoutSerializer(serializers.Serializer):
-    """
-    Сериализатор для logout'а пользователя,
-    токен заносится в black list
-    """
-    refresh = serializers.CharField()
-
-    default_error_message = {
-        'bad_token': ('Token is expired or invalid',)
-    }
-
-    def validate(self, attrs):
-        self.token = attrs['refresh']
-        return attrs
-
-    def save(self, **kwargs):
-        try:
-            RefreshToken(self.token).blacklist()
-        except TokenError:
-            self.fail('bad_token')
 
 
 class CreateCommonUserSerializer(serializers.ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -1,10 +1,9 @@
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from users.serializers import LoginSerializer, LogoutSerializer, \
-    CreateCommonUserSerializer
+from users.serializers import CreateCommonUserSerializer
 
 
 class CreateCommonUser(GenericAPIView):
@@ -20,31 +19,3 @@ class CreateCommonUser(GenericAPIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class LoginAPIView(GenericAPIView):
-    """
-    Авторизация пользователя и получение JWT токена
-    """
-    serializer_class = LoginSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class LogoutAPIView(GenericAPIView):
-    """
-    Logout пользователя и занесение токена в black list
-    """
-    serializer_class = LogoutSerializer
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
