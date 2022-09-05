374521e67f61d3d3ee0c4fbdb50dced6a0f7228c
Sergey Intyashev <seintyashev@mail.ru>
2022-09-05 13:22:40 +0400

    Add post filter

--- blogs/services.py
+++ blogs/services.py
@@ -1,3 +1,5 @@
+from django_filters import rest_framework as filters
+
 from blogs.models import Posts
 
 
@@ -7,3 +9,16 @@ def add_like_to_post(post: Posts) -> None:
     """
     post.likes += 1
     post.save()
+
+
+class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
+class PostFilter(filters.FilterSet):
+    tags = CharFilterInFilter(field_name='tags__title', lookup_expr='in')
+    created_at = filters.RangeFilter
+
+    class Meta:
+        model = Posts
+        fields = ['tags', 'created_at']
