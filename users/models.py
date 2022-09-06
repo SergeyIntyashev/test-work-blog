f6aea188957a5a7f8a84d766011da857142503a1
Sergey Intyashev <seintyashev@mail.ru>
2022-09-06 23:10:13 +0400

    Add property and has perm methods for admin panel

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -21,6 +21,20 @@ class CustomUser(AbstractBaseUser):
     USERNAME_FIELD = 'username'
 
     objects = CustomUserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
     def __str__(self):
         return self.username
