7b84e5dba4c678a14c27939932360116a0adc603
Sergey Intyashev <seintyashev@mail.ru>
2022-09-07 00:04:16 +0400

    Reorganize path

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,15 +4,15 @@ from rest_framework.routers import SimpleRouter
 from blogs import views
 
 urlpatterns = [
-    path('<int:pk>/add-authors', views.AddAuthorsToBlogView.as_view(),
+    path('blogs/<int:pk>/add-authors', views.AddAuthorsToBlogView.as_view(),
          name='add-authors-to-blog'),
-    path('<int:pk>/subscribe', views.SubscribeToBlogView.as_view(),
+    path('blogs/<int:pk>/subscribe', views.SubscribeToBlogView.as_view(),
          name='subscribe-to-blog'),
-    path('<int:pk>/posts', views.ListPostsOfBlogView.as_view(),
+    path('blogs/<int:pk>/posts', views.ListPostsOfBlogView.as_view(),
          name='posts-of-blog'),
-    path('my/favorites', views.ListFavoriteBlogsView.as_view(),
+    path('blogs/favorites', views.ListFavoriteBlogsView.as_view(),
          name='my-favorite-blogs'),
-    path('my/posts', views.ListUserPostsView.as_view(), name='my-posts'),
+    path('posts/my', views.ListUserPostsView.as_view(), name='my-posts'),
     path('posts/<int:pk>/add-comment', views.CreateCommentView.as_view(),
          name='create-comment-to-post'),
     path('posts/<int:pk>/add-like', views.LikePostView.as_view(),
@@ -22,7 +22,7 @@ urlpatterns = [
 router = SimpleRouter()
 router.register(r'comments', views.CommentView, basename='Comments')
 router.register(r'tags', views.TagsView, basename='Tags')
-router.register(r'', views.BlogsView, basename='Blogs')
+router.register(r'blogs', views.BlogsView, basename='Blogs')
 router.register(r'posts', views.PostsView, basename='Posts')
 
 urlpatterns += router.urls
