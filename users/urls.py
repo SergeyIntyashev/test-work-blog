a106d4be00c5f096ed562585fdd369ad1416dd03
Sergey Intyashev <seintyashev@mail.ru>
2022-09-06 12:02:43 +0400

    Rename view for sign-in

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from rest_framework_simplejwt.views import token_refresh, \
     token_obtain_pair, token_blacklist
 
-from users.views import CreateCommonUser
+from users.views import SignInView
 
 urlpatterns = [
-    path('sign-in', CreateCommonUser.as_view(), name='account-create'),
+    path('sign-in', SignInView.as_view(), name='account-create'),
     path('login', token_obtain_pair, name='login'),
     path('logout', token_blacklist, name='logout'),
     path('token/refresh', token_refresh, name='token-refresh'),
--- users/views.py
+++ users/views.py
@@ -1,19 +1,19 @@
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from users.serializers import CreateCommonUserSerializer
 
 
-class CreateCommonUser(GenericAPIView):
+class SignInView(CreateAPIView):
     """
     Создания простого пользователя
     """
     permission_classes = [AllowAny]
     serializer_class = CreateCommonUserSerializer
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
