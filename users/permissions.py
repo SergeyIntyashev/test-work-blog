de5e8b4aed3af55390ebb6876b6fd9cde007cfb3
Sergey Intyashev <seintyashev@mail.ru>
2022-09-07 23:23:33 +0400

    Fix IsAuthenticatedAndOwner permission

--- users/permissions.py
+++ users/permissions.py
@@ -9,6 +9,9 @@ class IsAuthenticatedAndOwner(BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
+        if isinstance(obj, Posts):
+            obj = obj.blog
+
         return bool(request.user and request.user.is_authenticated
                     and (obj.owner == request.user))
 
