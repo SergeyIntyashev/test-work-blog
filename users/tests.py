5f83daeff91724d5227dee1a7022a6097192b0cd
Sergey Intyashev <seintyashev@mail.ru>
2022-09-07 11:19:47 +0400

    Fix tests

--- users/tests.py
+++ users/tests.py
@@ -4,17 +4,20 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-def create_user(**params):
-    return get_user_model().objects.create_common_user(**params)
-
-
-account_create_url = reverse('account-create')
-login_url = reverse('login')
-blacklist_url = reverse('logout')
-token_refresh_url = reverse('token-refresh')
-
-
 class UserTests(APITestCase):
+
+    def setUp(self) -> None:
+        self.account_create_url = reverse('account-create')
+        self.login_url = reverse('login')
+        self.blacklist_url = reverse('logout')
+        self.token_refresh_url = reverse('token-refresh')
+
+        self.testuser_payload = {
+            'username': 'testuser1',
+            'password': 'testpassword123876521!'
+        }
+
+        get_user_model().objects.create_common_user(**self.testuser_payload)
 
     def test_create_valid_user(self):
         """Тестирование успешного создания пользователя"""
@@ -38,18 +41,11 @@ class UserTests(APITestCase):
 
         payload = {
             'username': 'testuser1',
-            'password': 'testpassword525529!'
-        }
-
-        create_user(**payload)
-
-        payload2 = {
-            'username': 'testuser1',
             'password': 'testpassword123876521!'
         }
 
         url = reverse('account-create')
-        response = self.client.post(url, payload2)
+        response = self.client.post(url, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -73,14 +69,7 @@ class UserTests(APITestCase):
     def test_create_token_for_user(self):
         """Тестирование авторизации пользователя"""
 
-        payload = {
-            'username': 'testuser1',
-            'password': 'testpassword525529!'
-        }
-
-        create_user(**payload)
-
-        response = self.client.post(login_url, payload)
+        response = self.client.post(self.login_url, self.testuser_payload)
         self.assertIn('access', response.data)
         self.assertIn('refresh', response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -93,7 +82,7 @@ class UserTests(APITestCase):
             'password': 'testpass!'
         }
 
-        response = self.client.post(login_url, payload)
+        response = self.client.post(self.login_url, payload)
 
         self.assertNotIn('access', response.data)
         self.assertNotIn('refresh', response.data)
@@ -104,7 +93,7 @@ class UserTests(APITestCase):
         Тестирование неудачной авторизации,
         из-за неверно заполненых учетных данных
         """
-        response = self.client.post(login_url,
+        response = self.client.post(self.login_url,
                                     {'email': 'one', 'password': ''})
 
         self.assertNotIn('access', response.data)
@@ -114,18 +103,12 @@ class UserTests(APITestCase):
     def test_refresh_token(self):
         """Тестирование получения access токена при помощи refresh токена"""
 
-        payload = {
-            'username': 'testuser1',
-            'password': 'testpassword525529!'
-        }
-
-        create_user(**payload)
-
-        response_login = self.client.post(login_url, payload)
+        response_login = self.client.post(self.login_url,
+                                          self.testuser_payload)
 
         payload = {'refresh': response_login.data['refresh']}
 
-        response = self.client.post(token_refresh_url, payload)
+        response = self.client.post(self.token_refresh_url, payload)
         self.assertIn('access', response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -134,7 +117,7 @@ class UserTests(APITestCase):
 
         payload = {'refresh': '123'}
 
-        response = self.client.post(token_refresh_url, payload)
+        response = self.client.post(self.token_refresh_url, payload)
         self.assertNotIn('access', response.data)
         self.assertIn('code', response.data)
         self.assertEqual(response.data['code'], 'token_not_valid')
@@ -145,7 +128,7 @@ class UserTests(APITestCase):
 
         payload = {'refresh': '123'}
 
-        response = self.client.post(token_refresh_url, payload)
+        response = self.client.post(self.token_refresh_url, payload)
         self.assertIn('code', response.data)
         self.assertEqual(response.data['code'], 'token_not_valid')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -153,16 +136,10 @@ class UserTests(APITestCase):
     def test_blacklist_token(self):
         """Тестирование удачного помещения токена в blacklist"""
 
-        payload = {
-            'username': 'testuser1',
-            'password': 'testpassword525529!'
-        }
-
-        create_user(**payload)
-
-        response_login = self.client.post(login_url, payload)
+        response_login = self.client.post(self.login_url,
+                                          self.testuser_payload)
 
         payload = {'refresh': response_login.data['refresh']}
 
-        response = self.client.post(token_refresh_url, payload)
+        response = self.client.post(self.token_refresh_url, payload)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
