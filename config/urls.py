10eaa828e3219db0f74af59973e45d41c5c76402
Sergey Intyashev <seintyashev@mail.ru>
2022-09-07 11:40:39 +0400

    Add static path

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg import openapi
@@ -25,4 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blogs.urls')),
     path('auth/', include('users.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
