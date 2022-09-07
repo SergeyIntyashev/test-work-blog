7d0adec0f9d8034e961fc1f67fdb0378f1dd9a61
Sergey Intyashev <seintyashev@mail.ru>
2022-09-07 17:13:30 +0400

    Fix IsAdminUser

--- users/permissions.py
+++ users/permissions.py
@@ -31,6 +31,9 @@ class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_admin)
 
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request=request, view=view)
+
 
 class IsAuthorOrBlogOwner(BasePermission):
     """
